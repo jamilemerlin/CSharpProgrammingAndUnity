09dd2a31fca68ae776d829df6c3623fd82f6b260
Jamile Merlin <jamilemerlin@gmail.com>
2020-06-15 21:30:18 +0200

    week 2
    add exercise8
    add exercise9

---
+++ exercises/week2/Exercise8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    /// <summary>
+    /// Exercise 8 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates rolling and using two dice
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // initialize random number generator
+            RandomNumberGenerator.Initialize();
+
+            // create two dice
+            Die die1 = new Die();
+
+            Die die2 = new Die();
+
+            // tell the dice to roll themselves
+
+            die1.Roll();
+            int d1 = die1.TopSide;
+
+            die2.Roll();
+            int d2 = die2.TopSide;
+
+            int sum = d1 + d2;
+
+            // print the top sides and the sum of the dice
+
+
+            Console.WriteLine("Die 1: " + d1);
+            Console.WriteLine("Die 2: " + d2);
+            Console.WriteLine("The sum the die 1 and die 2 is: " + sum);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ exercises/week2/Exercise9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    /// <summary>
+    /// Exercise 9 solution 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates using custom classes
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // create a new deck and tell the deck to print itself
+            Deck deck;
+            deck = new Deck();
+            // deck.Print();
+            // Console.WriteLine();
+
+            // tell the deck to shuffle and print itself
+            deck.Shuffle();
+            deck.Print();
+            Console.WriteLine();
+
+            // take the top card from the deck and print the card rank and suit
+            Card card0 = deck.TakeTopCard();
+
+            Console.WriteLine("First card is: " + card0.Rank + " of " + card0.Suit);
+
+            // take the top card from the deck and print the card rank and suit
+            Card card1 = deck.TakeTopCard();
+
+            Console.WriteLine("Second card is: " + card1.Rank + " of " + card1.Suit);
+            Console.WriteLine();
+        }
+    }
+}
