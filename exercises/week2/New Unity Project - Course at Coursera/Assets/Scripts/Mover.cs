cf46a53c10581a9387164991796552f2e2ac0386
Jamile Merlin <jamilemerlin@gmail.com>
2020-06-24 21:44:36 +0200

    week3 Course at Coursera

---
+++ exercises/week2/New Unity Project - Course at Coursera/Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Moves the game object
+/// </summary>
+public class Mover : MonoBehaviour
+{
+    /// <summary>
+    /// use this for inicialization
+    /// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the game object moving
+        Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.AddForce(new Vector2(1, 0), ForceMode2D.Impulse);
+        
+    }
+}
