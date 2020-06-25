d3725d40082f8204cc61bea3a43bb6a605c5ad03
Jamile Merlin <jamilemerlin@gmail.com>
2020-06-25 20:10:32 +0200

    exercise with IfStatement and Exercise14 to apply object game moviment

---
+++ exercises/week3/Exercise14/Assets/Scripts/Jumper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves the game object
+/// </summary>
+public class Jumper : MonoBehaviour
+{
+
+    //  jump  location  support 
+    const float maxX = 5;
+    const float minX = -5;
+    const float maxY = 2.5f;
+    const float minY = -2.5f;
+
+    //  timer  support
+    const float TotalJumpDelaySeconds = 1;
+    float elapsedJumpDelaySeconds = 0;
+
+    void Update()
+    {
+        //  update  timer  and  check  if  it's done
+        elapsedJumpDelaySeconds += Time.deltaTime;
+
+        if (elapsedJumpDelaySeconds >= TotalJumpDelaySeconds)
+        {
+            elapsedJumpDelaySeconds = 0;
+            transform.position = new Vector3(Random.Range(maxX, minX), Random.Range(maxY, minY), 0);
+        }
+    }
+}
---
+++ exercises/week3/IfStatement/IfStatement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IfStatements
+{
+    /// <summary>
+    /// If Statements Lecture code
+    /// </summary>
+    class MainClass
+    {
+        /// <summary>
+        /// Demonstrates various forms of if statemented
+        /// </summary>
+        /// <param name="args">comand-line arguments</param>
+        public static void Main(string[] args)
+        {
+            // prompt for and get user input
+            Console.Write("Pick up the shine thing? 1 for yes, 2 for no: ");
+            int answer = int.Parse(Console.ReadLine());
+
+            // print appropriate message
+
+            if (answer == 1)
+            {
+                Console.WriteLine("You have the shine thing");
+            }
+            else if (answer == 2)
+            {
+                Console.WriteLine("You don't have the shine thing");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number!");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
