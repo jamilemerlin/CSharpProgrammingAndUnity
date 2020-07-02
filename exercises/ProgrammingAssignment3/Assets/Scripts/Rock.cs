a2c447b58df54db67cb9181ac3e857ce4d1b553f
Jamile Merlin <jamilemerlin@gmail.com>
2020-07-02 21:34:53 +0200

    Programming Assignment 3
    Instantiate
    Serialize
    Spawn
    Destroy
    Find object by tag

---
+++ exercises/ProgrammingAssignment3/Assets/Scripts/Rock.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        // apply impulse force to get game object moving
+        const float MinImpulseForce = 2f;
+        const float MaxImpulseForce = 6f;
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector2 direction = new Vector2(
+            Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(
+            direction * magnitude,
+            ForceMode2D.Impulse);
+    }
+
+    /// <summary>
+    /// Destroy the rocks
+    /// </summary>
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// when the rock goes out of the screen, it will be destroy
+    /// </summary>
+    void OnBecameInvisible()
+    {
+        this.Destroy();
+    }
+
+
+    /// <summary>
+    /// Called on a colision
+    /// </summary>
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // when the rock hit another rock, it will be destroy
+        if (coll.gameObject.tag == "RockPrefab")
+        {
+            this.Destroy();
+        }
+    }
+
+    void Update()
+    {
+   
+    }
+
+}
---
+++ exercises/ProgrammingAssignment3/Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour
+{
+    // for spawning
+    [SerializeField]
+    GameObject GreenRockPrefab;
+    [SerializeField]
+    GameObject MagentaRockPrefab;
+    [SerializeField]
+    GameObject WhiteRockPrefab;
+
+    // delay spawn control
+    const float MinSpawnDelay = 1;
+    const float MaxSpawnDelay = 2;
+    Timer spawnTimer;
+
+    // spawn location support
+    const int SpawnBorderSize = 100;
+    int minSpawnX;
+    int maxSpawnX;
+
+    int minSpawnY;
+    int maxSpawnY;
+
+    /// <summary>
+    /// This for inicialization
+    /// </summary>
+    void Start()
+    {
+        // spawn boundaries
+        minSpawnX = SpawnBorderSize;
+        maxSpawnX = Screen.width - SpawnBorderSize;
+
+        minSpawnY = SpawnBorderSize;
+        maxSpawnY = Screen.height - SpawnBorderSize;
+
+        // create and start timer
+        spawnTimer = gameObject.AddComponent<Timer>();
+        spawnTimer.Duration = Random.Range(MinSpawnDelay, MaxSpawnDelay);
+        spawnTimer.Run();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        // check for time to spawn a new Teddy bear
+        if (spawnTimer.Finished)
+        {
+            SpawnRock();
+
+            // change spanw timer duration and restart
+            spawnTimer.Duration = Random.Range(MinSpawnDelay, MaxSpawnDelay);
+            spawnTimer.Run();
+        }
+
+    }
+
+    void SpawnRock()
+    {
+        // generate random location
+        Vector3 location = new Vector3(Random.Range(minSpawnX, maxSpawnX),
+            Random.Range(minSpawnY, maxSpawnY),
+            -Camera.main.transform.position.z);
+        Vector3 worldLocation = Camera.main.ScreenToWorldPoint(location);
+
+        int totalRocks = GameObject.FindGameObjectsWithTag("RockPrefab").Length;
+
+        if (totalRocks >= 3)
+        {
+            return;
+        }
+
+        // spawn random prefab
+        int prefabNumber = Random.Range(0, 3);
+        
+        if (prefabNumber == 0)
+        {
+            Instantiate<GameObject>(GreenRockPrefab, worldLocation, Quaternion.identity);
+        }
+        else if (prefabNumber == 1)
+        {
+            Instantiate<GameObject>(WhiteRockPrefab, worldLocation, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate<GameObject>(MagentaRockPrefab, worldLocation, Quaternion.identity);
+        }
+    }
+}
