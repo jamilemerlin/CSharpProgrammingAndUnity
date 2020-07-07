f4fcdb4c430c31e5f14b32af765a07d9468071d0
Jamile Merlin <jamilemerlin@gmail.com>
2020-07-07 22:05:21 +0200

    week 4
    Mouse location processing
    Clampdown
    organization with region

---
+++ exercises/week4/exercise18/Assets/Scripts/CharacterChanger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterChanger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject BurningTeddyPrefab;
+    [SerializeField]
+    GameObject TeddyBearPrefab;
+
+    // need for a location of new character
+    GameObject currentCharacter;
+
+    // identify the character
+    bool teddyIsBurning = false;
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        currentCharacter = Instantiate<GameObject>(
+            TeddyBearPrefab, Vector3.zero, Quaternion.identity);
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        //change character on left mouse buttom 
+        if (Input.GetMouseButtonDown(0))
+        {
+            // save current position and destroy current character
+            Vector3 position = currentCharacter.transform.position;
+            Destroy(currentCharacter);
+
+            // selecting the new character
+            this.teddyIsBurning = !this.teddyIsBurning;
+            if (this.teddyIsBurning)
+            {
+                currentCharacter = Instantiate(
+                    BurningTeddyPrefab, position, Quaternion.identity);
+            }
+            else
+            {
+                currentCharacter = Instantiate(
+                    TeddyBearPrefab, position, Quaternion.identity);
+            }
+        }
+    }
+}
