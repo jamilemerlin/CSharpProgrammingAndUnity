24cd637c7b71b38eae5a2b1bd8e5a3a0a39b2c07
Jamile Merlin <jamilemerlin@gmail.com>
2020-07-27 18:44:19 +0200

    exercise 19
    - Use input axis to handle left and right mouse click
    - add/remove teddybears based on input axis

---
+++ exercises/week4/Exercise19/Assets/scripts/MouseButtonProcessor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Processes mouse button inputs
+/// </summary>
+public class MouseButtonProcessor : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefabExplosion;
+    [SerializeField]
+    GameObject prefabTeddyBear;
+
+    // first frame input support
+    bool spawnInputOnPreviousFrame = false;
+	bool explodeInputOnPreviousFrame = false;
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+        // spawn teddy bear as appropriate
+        if (Input.GetAxis("SpawnTeddyBear") > 0)
+        {
+            if (!spawnInputOnPreviousFrame)
+            {
+                
+                spawnInputOnPreviousFrame = true;
+
+                Vector3 position = Input.mousePosition;
+                position.z = -Camera.main.transform.position.z;
+                position = Camera.main.ScreenToWorldPoint(position);
+
+                GameObject.Instantiate(
+                        prefabTeddyBear, position, Quaternion.identity);
+            }
+        }
+        else
+        {
+            spawnInputOnPreviousFrame = false;
+        }
+        // explode teddy bear as appropriate
+        if (Input.GetAxis("ExplodeTeddyBear") > 0)
+        {
+            if (!explodeInputOnPreviousFrame)
+            {
+
+                explodeInputOnPreviousFrame = true;
+
+                GameObject teddyBear = GameObject.FindWithTag("TeddyBear");
+
+                if (teddyBear != null)
+                {
+                    Destroy(teddyBear);
+                    GameObject.Instantiate(
+                            prefabExplosion, teddyBear.transform.position, Quaternion.identity);
+                }
+                
+            }
+        }
+        else
+        {
+            explodeInputOnPreviousFrame = false;
+        }
+    }
+}
